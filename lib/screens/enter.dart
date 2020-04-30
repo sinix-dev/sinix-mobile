a1546b36f5e0e50a51400fd1d9f4232cdf2bd9c3
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-04-30 17:30:52 +0530

    EntryPage UI :check:

--- lib/screens/enter.dart
+++ lib/screens/enter.dart
@@ -10,11 +10,41 @@ class _EnterPageState extends State<EnterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        height: 500,
         child: Center(
-          child: RaisedButton(
-            onPressed: (){},
-            child: Text("Connect!"),
-          )
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: Text(
+                  "SINIX",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 70
+                  ),
+                )
+              ),
+              SizedBox(
+                height: 15
+              ),
+              Container(
+                width: 300,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Sinix Game IP Address"
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              RaisedButton(
+                onPressed: (){},
+                child: Text("Connect!"),
+              )
+            ],
+          ),
         )
       ),
     );
