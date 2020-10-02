355ccddcaa4ebb7e4a63a588e97ed816aea4d2ff
ritvij14 <ritvij2001@gmail.com>
2020-10-02 10:04:35 +0530

    added username in the payload

--- lib/pages/game.dart
+++ lib/pages/game.dart
@@ -39,6 +39,7 @@ class GamePage extends StatelessWidget {
                               "payload": {
                                 "x": delta.dx,
                                 "y": delta.dy,
+                                "username": user.username,
                               }
                             };
 
