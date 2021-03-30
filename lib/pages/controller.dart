c49aa4890f765e18c2aac14210dbfecd60599197
ritvij14 <ritvij2001@gmail.com>
2021-03-30 08:42:32 +0530

    added gyroscope value event

--- lib/pages/controller.dart
+++ lib/pages/controller.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:feather_icons_flutter/feather_icons_flutter.dart';
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
+import 'package:sensors/sensors.dart';
 
 import '../blocs/blocs.dart';
 import '../utils/config.dart';
@@ -8,9 +11,25 @@ import '../models/controller.dart';
 
 import 'controller/index.dart';
 
-class ControllerPage extends StatelessWidget {
+class ControllerPage extends StatefulWidget {
+  @override
+  _ControllerPageState createState() => _ControllerPageState();
+}
+
+class _ControllerPageState extends State<ControllerPage> {
+  List<double>? _gyroscopeValues;
+  List<StreamSubscription<dynamic>> _streamSubscriptions =
+      <StreamSubscription<dynamic>>[];
+
   @override
   Widget build(BuildContext context) {
+    final List<String>? gyroscope =
+        _gyroscopeValues?.map((double v) => v.toStringAsFixed(1)).toList();
+    if (gyroscope != null) {
+      for (String value in gyroscope) {
+        Blocs.channelBloc.udpChannel.add(value);
+      }
+    }
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
       body: StreamBuilder<List<ControllerModel>>(
@@ -61,5 +80,23 @@ class ControllerPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    for (StreamSubscription<dynamic> subscription in _streamSubscriptions) {
+      subscription.cancel();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _streamSubscriptions.add(gyroscopeEvents.listen((GyroscopeEvent event) {
+      setState(() {
+        _gyroscopeValues = <double>[event.x, event.y, event.z];
+      });
+    }));
   }
 }
